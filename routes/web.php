e366f0da2c3086e09676c0eaf2bc33ae8101cafb
Joao Pedro <sk8.99.jpedro@gmail.com>
2024-12-11 15:01:19 -0300

    TESTES DE CODIGO

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+
+
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/empresa', function () {
+//     return view('empresa');
+// });
+
+// Route::view('/empresa', 'empresa');
+
+// ----Agrupando usando rotas----
+// Route::group(['prefix'=>'admin', 'as' => 'admin.']function(){
+//     Route::get('exemple0', function(){
+//         return "exemple0";
+//     });
+//     Route::get('exemple01', function(){
+//         return "exemple 01";     
+//     });
+//     Route::get('exemple02', function(){
+//         return "exemple 02";
+        
+//     });
+// });
+
+/*  -- Exemplo de Rotas --
+Route::any('/any', function(){
+    return "Permite todo tipo de acesso http (put, delete,get,post)";
 });
+
+Route::match(['get','post'] ,'/match', function(){
+    return "permite apenas acessos definidos ";
+});
+*/
+
+// --  Passando parametros pela uri  --
+// Route::get('/produto/{id}', function($id){
+//     return "O id do produto é ".$id;
+// });
+
+
